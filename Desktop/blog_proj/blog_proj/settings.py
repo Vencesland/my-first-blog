fcdaf688aa010dc316bc2acee49efb1c9cedbc19
ian <ianneri42@gmail.com>
2019-06-24 10:52:11 -0300

    new api

--- Desktop/blog_proj/blog_proj/settings.py
+++ Desktop/blog_proj/blog_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9caed2000f975583d034df396646fa96b0ec8d08'
+SECRET_KEY = '7eb80d7ab107127478826a25e001f122d05cd489'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
